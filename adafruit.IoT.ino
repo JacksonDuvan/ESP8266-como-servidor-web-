bbf297105b82a4a3711539d591413ede5cf28b27
Electron <31048230+JacksonDuvan@users.noreply.github.com>
2017-11-08 15:28:57 -0500

    Add files via upload

---
+++ adafruit.IoT.ino
@@ -0,0 +1,185 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "ARRIS-2692"
+#define WLAN_PASS       "32352DC099DB705D"
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "jzambranosb"
+#define AIO_KEY         "35e02be7d243465f8b4791a5544c3780"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish temperatura = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperatura");
+Adafruit_MQTT_Publish pulsador = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/pulsador");
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe salidaDigital = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/salidaDigital");
+Adafruit_MQTT_Subscribe salidaAnalogica = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/salidaAnalogica");
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+void digitalCallback(char *data, uint16_t len) {
+  Serial.print("Hey we're in a onoff callback, the button value is: ");
+  Serial.println(data);
+
+     String message = String(data);
+      message.trim();
+      if (message == "ON") {digitalWrite(12, HIGH);}
+      if (message == "OFF") {digitalWrite(12, LOW);} 
+}
+
+void analogicaCallback(char *data, uint16_t len) {
+  Serial.print("Hey we're in a onoff callback, the button value is: ");
+  Serial.println(data);
+
+     String message = String(data);
+     message.trim();
+     analogWrite(13, message.toInt());
+}
+int contconexion = 0;
+
+unsigned long previousMillis = 0;
+
+char charPulsador [15];
+String strPulsador;
+String strPulsadorUltimo;
+
+void setup() {
+
+  //prepara GPI13 y 12 como salidas 
+  pinMode(13, OUTPUT); // D7 salida analógica
+  analogWrite(13, 0); // analogWrite(pin, value);
+  pinMode(12, OUTPUT); // D6 salida digital
+  digitalWrite(12, LOW);
+
+  // Entradas
+  pinMode(14, INPUT); // D5
+  
+  Serial.begin(9600);
+  delay(10);
+
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for onoff feed.
+  salidaDigital.setCallback(digitalCallback);
+  salidaAnalogica.setCallback(analogicaCallback);
+  mqtt.subscribe(&salidaDigital);
+  mqtt.subscribe(&salidaAnalogica);
+}
+
+uint32_t x=0;
+
+void loop() {
+  
+   MQTT_connect();
+
+  unsigned long currentMillis = millis();
+    
+  if (currentMillis - previousMillis >= 2000) { //envia la temperatura cada 2 segundos
+    previousMillis = currentMillis;
+    int analog = analogRead(17);
+    float temp = analog*0.322265625;
+    Serial.print(F("\nSending temperatura val "));
+    Serial.print(temp);
+    Serial.print("...");
+    if (! temperatura.publish(temp)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+    }
+  }
+
+  if (digitalRead(14) == 0) {
+    strPulsador = "presionado";
+  } else {
+    strPulsador = "NO presionado";
+  }
+
+  if (strPulsador != strPulsadorUltimo) { //envia el estado del pulsador solamente cuando cambia.
+    strPulsadorUltimo = strPulsador;
+    strPulsador.toCharArray(charPulsador, 15);
+    Serial.print(F("\nSending pulsador val "));
+    Serial.print(strPulsador);
+    Serial.print("...");
+    if (! pulsador.publish(charPulsador)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+    }
+    delay(1000);
+  }
+
+  // this is our 'wait for incoming subscription packets and callback em' busy subloop
+  // try to spend your time here:
+  mqtt.processPackets(500);
+  
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  
+  // if(! mqtt.ping()) {
+  //   mqtt.disconnect();
+  // }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
